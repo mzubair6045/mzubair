d1691d0901a3d2bfc5d1e4d7d2a2c89ec164ab2c
mzubair6045 <99383327+mzubair6045@users.noreply.github.com>
2026-01-03 13:33:51 -0500

    Create main.js

---
+++ main.js
@@ -0,0 +1,8 @@
+function scrollToContact() {
+  window.location.href = "contact.html";
+}
+
+function submitForm(event) {
+  event.preventDefault();
+  alert("Thank you! Your message has been sent.");
+}
